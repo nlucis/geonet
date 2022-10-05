4ebd08069c9923bfe0eb0540c5f51da3da96c5e1
Arjay Jones <arjay.jones@nlucis.dev>
2022-10-04 18:27:06 -0700

    removed leaflet-providers and stamen JS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import * as PIXI from 'pixi.js';
-import * as L from 'leaflet';
+import PIXI from 'pixi.js';
+import LEAF from 'leaflet';
+import WinBox from 'winbox';
+
 import * as data from './data/geomapData.json';
 
-const geomap = L.map('geomap').setView([43.659752, -79.378161], 20);
+const geomap = LEAF.map('geomap').setView([43.659752, -79.378161], 20);
 
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
-const Stamen_Toner = L.tileLayer(data.tileURI, 
+const Stamen_Toner = LEAF.tileLayer(data.tileURI, 
 	{
 		attribution: data.attribution,
 		subdomains: 'abcd',
@@ -26,3 +28,6 @@ L.circleMarker([43.659752, -79.378161]).addTo(geomap)
 		'Tel: 416-9795000 Ext. 5192'
 	)
 	.openPopup();
+
+	const ungabox = new WinBox('ungabunga');
+	ungabox.show();
