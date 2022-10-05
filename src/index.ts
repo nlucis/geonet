35b5cc6e9e58cbb2ceddb8b97f1eff52e5b58eaf
Arjay Jones <arjay.jones@nlucis.dev>
2022-10-04 17:17:35 -0700

    added leaflet and pixi; included script to copy over leaflet css from node module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-console.log('shucks');
+import * as PIXI from 'pixi.js';
+import * as LEAF from 'leaflet';
 
-export default {};
+const geomap = LEAF.map('geomap').setView([43.659752, -79.378161], 20);
+
+// Adds the basemap tiles to your web map
+// Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
+LEAF.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: 
+		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 21
+}).addTo(geomap);
+
+// Adds a popup marker to the webmap for GGL address
+LEAF.circleMarker([43.659752, -79.378161]).addTo(geomap)
+	.bindPopup(
+		'MON 304<br>' + 
+		'Monetary Times Building<br>' +
+		'341 Victoria Street<br>' + 
+		'Toronto, Ontario, Canada<br>' +
+		'M5B 2K3<br><br>' + 
+		'Tel: 416-9795000 Ext. 5192'
+	)
+	.openPopup();
