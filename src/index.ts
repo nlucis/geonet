30c66fb6c396036ca7de570109d6569e097a581c
Arjay Jones <arjay.jones@nlucis.dev>
2022-10-04 17:59:23 -0700

    converted to using JSON for storing map config data; added PNG directly to URI and removed the ext TileLayer option since it was not declared via interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import * as PIXI from 'pixi.js';
 import * as L from 'leaflet';
+import * as data from './data/geomapData.json';
 
 const geomap = L.map('geomap').setView([43.659752, -79.378161], 20);
 
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
-const Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 18,
-	ext: 'png'
+const Stamen_Toner = L.tileLayer(data.mapdataURI, 
+	{
+		attribution: data.attribution,
+		subdomains: 'abcd',
+		minZoom: 0,
+		maxZoom: 18
 });
 Stamen_Toner.addTo(geomap);
 
